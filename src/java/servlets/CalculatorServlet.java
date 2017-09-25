739ba4dba05f3bfe5f8281decd9b6314d01c1cff
727334 <727334@ELF6IT215939.ACDM.DS.SAIT.CA>
2017-09-25 09:34:58 -0600

    Complete Week 3 lab 2 complete

---
+++ src/java/servlets/CalculatorServlet.java
@@ -0,0 +1,96 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package servlets;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+public class CalculatorServlet extends HttpServlet {
+
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        
+         
+        
+
+        String firstStr = request.getParameter("first");
+
+        String secondStr = request.getParameter("second");
+
+        String operation = request.getParameter("operation");
+
+
+        int result=0;
+        
+        
+        boolean calcPerformed=false;
+
+
+        if (operation == null)
+        {
+            request.setAttribute("errorMessage", "Please Enter a Value");
+        getServletContext().getRequestDispatcher("/WEB-INF/calculator.jsp").forward(request, response);
+        return;
+        }
+        
+
+        if (firstStr!=null && secondStr!=null && !firstStr.equals("") && !secondStr.equals("")){
+
+        int first = Integer.parseInt(firstStr);
+
+        int second = Integer.parseInt(secondStr);
+
+
+        char operationType = operation.charAt(0);
+
+
+        switch (operationType)
+
+        {
+
+        case '+': result = first + second;
+
+        break;
+
+        case '-': result = first - second;
+
+        break;
+
+        case '*': result = first * second;
+
+        break;
+
+        case '%': result = first % second;
+
+        break;
+
+        }
+
+
+        calcPerformed = true;   
+        request.setAttribute("result", result);
+        getServletContext().getRequestDispatcher("/WEB-INF/calculator.jsp").forward(request, response); 
+        }
+
+        
+
+    }
+
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        
+    }
+
+}
